879a2a4a5571862e003c63571185785c4acaaea2
Rodrigo Sena FIAP <RM360108@fiap.com.br>
2024-12-09 17:29:02 -0300

    aumentado os fontes e centralizado na tela

--- ScriptTesteAutoScaling.sh
+++ ScriptTesteAutoScaling.sh
@@ -17,7 +17,7 @@ systemctl enable httpd
 EC2AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
 # Cria um arquivo temporário com o template HTML
-echo '<center><h1>Esta EC2 está na Zona: AZID </h1></center>' > /var/www/html/index.txt
+echo '<html><head><style>body { display: flex; justify-content: center; align-items: center; height: 100vh; background-color: #f0f0f0; } h1 { font-size: 48px; color: #333; }</style></head><body><h1>Esta EC2 está na Zona: AZID</h1></body></html>' > /var/www/html/index.txt
 
 # Substitui o placeholder AZID pela zona real e cria o arquivo HTML final
 sed "s/AZID/$EC2AZ/" /var/www/html/index.txt > /var/www/html/index.html
